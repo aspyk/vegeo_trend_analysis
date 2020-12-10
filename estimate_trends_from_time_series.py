Simplify code and fix the slow constant array computation bug

estimate_trends_from_time_series.py
@@ -19,7 +19,9 @@ from time import sleep
 import sys, glob, os
 import traceback
 from timeit import default_timer as timer
+import datetime
 import psutil
+import pathlib
 
 
     
@@ -38,46 +40,67 @@ def init_worker_nc(var_dict, file_timeseries, file_outpath):
 def processInput_trends(subchunk, parent_iteration, child_iteration):
     """This is the main file that calculate trends"""
 
+    #print('INFO: see pid.<pid>.out to monitor trend computation progress')
+    #sys.stdout = open('pid'+str(os.getpid()) + '.out', 'w')
+    #sys.stdout = open('pid.out', 'a')
+
+    print ('### Chunk {} > subchunk {} started: COL: [{}:{}] ROW: [{}:{}]'.format(parent_iteration, child_iteration, *subchunk.get_limits('local', 'str')))
+
     ## Debug tool to print process Ids
     process = psutil.Process(os.getpid())
     current = current_process()
     #print(current._identity, f'{process.memory_info().rss/1024/1024} Mo')
 
-    # Read the input time series file from main chunk, configured length of time X 500 X 500; it may vary if different chunks are used
+    write_string0 = (param.hash+"_CHUNK_" + np.str(parent_iteration)  
+                     + "_SUBCHUNK_" + np.str(child_iteration)   
+                     + "_" + '_'.join(subchunk.get_limits('global', 'str'))
+                     + '.nc')
+    subchunk_fname = var_dict['file_output_tendencies'] + '/' + write_string0
+    subchunk_fname = os.path.normpath(subchunk_fname)
+
+    ## Check if cache file already exists and must be overwritten
+    if not param.b_delete:
+        if pathlib.Path(subchunk_fname).is_file():
+            print ('Subchunk {} already exists in {}. Use -d option to overwrite it.'.format(child_iteration, write_string0))
+            return
+
+    ## Read the input time series file from main chunk, configured length of time X 500 X 500; it may vary if different chunks are used
     hdf_ts = Dataset(var_dict['file_input_time_series'], 'r', format='NETCDF4')
 
-    # Create temporary storage with size of sub chunks in main chunk, currently configured 100 by 100 blocks
+    ## Create temporary storage with size of sub chunks in main chunk, currently configured 100 by 100 blocks
     var_temp_output = np.empty([*subchunk.dim,4])    
     var_temp_output[:] = np.nan
     # NaN matrix by default
-    print ('### Chunk {} > subchunk {} started: COL: [{}:{}] ROW: [{}:{}]'.format(parent_iteration, child_iteration, *subchunk.get_limits('local', 'str')))
 
     
+    ## Parameters for te loop
     b_deb = 0 # flag to print time profiling
     t00 = timer()
     t000 = timer()
     t_mean = 0.
     #print(current._identity, f'{process.memory_info().rss/1024/1024} Mo')
-    #for ii_sub in range(*subchunk.get_limits('local', 'tuple')[:2]):
     offsetx = subchunk.get_limits('local', 'tuple')[0]
     offsety = subchunk.get_limits('local', 'tuple')[2]
 
+    print_freq = 20
+
+    tab_prof_valid = []
+    tab_prof_zero = []
+
     for jj_sub in range(subchunk.dim[0]):
+    #for jj_sub in range(28,30): #debug
         # dimension of variable: time,x,y
         # preload all the y data here to avoid overhead due to calling Dataset.variables at each iteration in the inner loop
-        data_test0 = hdf_ts.variables['time_series_chunk'][:,jj_sub+offsety,:]
+        data_test0 = hdf_ts.variables['time_series_chunk'][:,jj_sub+offsety,offsetx:offsetx+subchunk.dim[1]]
         #data_test0 = hdf_ts.variables['time_series_chunk'][:500,sub_chunks_x[ii_sub],:]
-        #print(f'ii_sub:{ii_sub} ({sub_chunks_x[0]},{sub_chunks_x[-1]})', f'len(sub_chunks_y):{len(sub_chunks_y)} ({sub_chunks_y[0]},{sub_chunks_y[-1]})')
-        # debug
-        #print(np.count_nonzero(np.isnan(data_test0)))
 
         for ii_sub in range(subchunk.dim[1]):
             if b_deb: print('---------------')
-            if b_deb: print(f'{ii_sub} ({sub_chunks_x[0]},{sub_chunks_x[-1]})', f'{jj_sub} ({sub_chunks_y[0]},{sub_chunks_y[-1]})')
+            if b_deb: print('jj: {} - ii: {} '.format(jj_sub, ii_sub))
             
             t0 = timer()
 
-            data_test = data_test0[:,ii_sub+offsetx]
+            data_test = data_test0[:,ii_sub]
     
 
             #data_test=hdf_ts.variables['time_series_chunk'][:,sub_chunks_x[ii_sub],sub_chunks_y[jj_sub]]
@@ -90,10 +113,6 @@ def processInput_trends(subchunk, parent_iteration, child_iteration):
             if b_deb: print('Data:', f'{data_test.size - np.isnan(data_test).sum()}/{data_test.size}')
             
             if 0:
-            #if (data_test.size - np.isnan(data_test).sum())>0: # test if there are not only NaNs (12 threshold has been removed here)
-            #if (data_test.size - np.isnan(data_test).sum())>12: # test if there is at least 12 numbers (why 12 here ?...)
-            #if len(np.where(np.isnan(data_test)!=1)[0])>12: # previous version 3 ot 4 times slower
-
                 #print('Use mstats')
 
                 data_sen=np.ma.masked_array(data_test, mask=np.isnan(data_test))
@@ -123,15 +142,31 @@ def processInput_trends(subchunk, parent_iteration, child_iteration):
             already imported'''
             if 1:
                 ## orinal mann-kendall test :
-                p,z,Sn,nx = m.mk_trend(len(data_test), np.arange(len(data_test)), data_test)
-                # if data_test = [], the test return (p,z,Sn,nx) = (1.0, 0.0, 0.5, 0.0)
+                bla = data_test[data_test!=999]
+                if bla.size > 0:
+                    #print('min/mean/max', bla.min(), bla.mean(), bla.max(), len(bla))
+                    if len(np.unique(bla))==1:
+                        p,z,Sn,nx = [0,0,0,0] 
+                    else:
+                        #data_test = data_test[-10:] # debug line to speed up
+                        p,z,Sn,nx = m.mk_trend(len(data_test), np.arange(len(data_test)), data_test)
+                else:
+                    p,z,Sn,nx = m.mk_trend(len(data_test), np.arange(len(data_test)), data_test)
+                    # if data_test = [], the test return (p,z,Sn,nx) = (1.0, 0.0, 0.5, 0.0)
             else:
                 ## other test
                 p,z,Sn,nx = [0,0,0,0] 
                 z = data_test.mean()
 
             if b_deb:
-                print('t4', timer()-t0)
+                t4 = timer()-t0
+                if bla.size>0:
+                    if bla.mean()==0.0:
+                        tab_prof_zero.append(t4)
+                    else:
+                        tab_prof_valid.append(t4)
+                    
+                print(t4)
                 t0 = timer()
 
             if b_deb: print('p,z,slope,nx', p,z,slope,nx)
@@ -142,6 +177,18 @@ def processInput_trends(subchunk, parent_iteration, child_iteration):
             var_temp_output[jj_sub,ii_sub,2] = Sn
             var_temp_output[jj_sub,ii_sub,3] = nx
 
+       
+        ## Print efficiency stats
+        if (jj_sub+1)%print_freq==0:
+            elapsed = timer()-t00
+            data_stat = hdf_ts.variables['time_series_chunk'][:,jj_sub+1+offsety-print_freq:jj_sub+1+offsety,offsetx:offsetx+subchunk.dim[1]]
+            valid = 100.*(data_stat.size - np.count_nonzero(np.isnan(data_stat)))/data_stat.size
+            eff = 1e6*elapsed/data_stat.size
+            #print(subchunk.dim, data_test0.shape)
+            print('{} : {}.{}.block[{}-{}] : {:.3f}s elapsed : {:.3f} us/pix/date : {:.2f}% valid'.format(datetime.datetime.now(), parent_iteration, child_iteration, jj_sub+1-print_freq, jj_sub+1, elapsed, eff, valid))
+
+            t00 = timer()
+
         if 0:
             t_mean += timer()-t00
             #print(f't00 {ii_sub} {t_mean/(ii_sub+1)}')
@@ -151,21 +198,25 @@ def processInput_trends(subchunk, parent_iteration, child_iteration):
                 print(f'{np.count_nonzero(np.isnan(v[:,ii]))/v[:,ii].size:.3f}', np.nanmin(v[:,ii]), np.nanmax(v[:,ii]))
             print(np.nanmin(v), np.nanmax(v))
             t00 = timer()
-    
-    #print(f't000tot.p{current._identity[0]} {timer()-t000:.3f}s {process.memory_info().rss/1024/1024:.2f}Mo')
-    #print ('<<< Block completed: ROW: [{}:{}] COL: [{}:{}]'.format(tuple_limits1, tuple_limits1+step_chunk1, tuple_limits2, tuple_limits2+step_chunk2))
-    write_string0 = (param.hash+"_CHUNK_" + np.str(parent_iteration)  
-                     + "_SUBCHUNK_" + np.str(child_iteration)   
-                     + "_" + '_'.join(subchunk.get_limits('global', 'str'))
-                     + '.nc')
-    write_string = var_dict['file_output_tendencies'] + '/' + write_string0
-    write_string = os.path.normpath(write_string)
-    # var_dict['file_output_tendencies'] prepared from the initializer routes the tendencies files to output destination as defined by product_tag previously
+   
+    if b_deb:
+    #if 1:
+        valid = np.array(tab_prof_valid)
+        zero = np.array(tab_prof_zero)
+
+        #print('valid:', valid.size, valid.min(), valid.mean(), valid.max())
+        #print('zero:', zero.size, zero.min(), zero.mean(), zero.max())
+        print(valid.mean())
+        print(zero.mean())
+        #return
+        #sys.exit()
+
+    print(f't000tot.p{current._identity[0]} {timer()-t000:.3f}s {process.memory_info().rss/1024/1024:.2f}Mo')
 
     if lock.acquire():
         #print ('Pool process Thread Locked:')
         #print ('Pool process Writing NetCDF:')
-        nc_output = Dataset(write_string, 'w')
+        nc_output = Dataset(subchunk_fname, 'w')
         xdim = nc_output.createDimension('X_dim', subchunk.dim[1])
         ydim = nc_output.createDimension('Y_dim', subchunk.dim[0])
         Vardim = nc_output.createDimension('Var', 4)
@@ -190,9 +241,6 @@ def processInput_trends(subchunk, parent_iteration, child_iteration):
     
         print ('Subchunk {} completed, save to {}'.format(child_iteration, write_string0))
         
-        with open(os.path.normpath(var_dict['file_output_tendencies'] + '/filelist.txt'),'a') as fl:
-            fl.write(write_string0+'\n')
-        
         return None 
 
 
@@ -200,18 +248,12 @@ def processInput_trends(subchunk, parent_iteration, child_iteration):
 def main():
 
     input_path = param.input
-
     output_path = param.output
-    
     product_tag = param.product_tag
     
     inpath_final = os.path.normpath(input_path+os.sep + product_tag + os.sep) + os.sep
     outpath_final = os.path.normpath(output_path + os.sep + product_tag + os.sep) + os.sep
 
-    # Empty list of data files used further for plotting
-    with open(outpath_final + '/filelist.txt', 'w'): pass
-
-    
     nchild = 100
     main_block_iteration = 0
     for chunk in param.chunks.list:
@@ -222,28 +264,36 @@ def main():
         print(in_file)
         print('***Row/y_SIZE***', chunk.dim[0], '***Col/x_SIZE***', chunk.dim[1]) 
         
-         
         chunk.subdivide(nchild)
-        #for sub in chunk.list:
-        #    print(sub.dim)
 
         result_chunks = [(sub, main_block_iteration, i) for i,sub in enumerate(chunk.list)]
-    
-        # Applying the multiple processing here, with process of choice, i use 4 for local and 16 for lustre 
-        # Here we pass the arguments to the initializer for pool so we use in the function used in multiple processing, it can be changed differently 
-        with Pool(processes=param.nproc, initializer=init_worker_nc, initargs=(var_dict, in_file, outpath_final)) as pool:
-            # I am not returning results, usually you can return and write the results after multiprocessing
-            print('pool started')
-            results = pool.starmap(processInput_trends, [(i) for i in result_chunks])
-            pool.close()
-            pool.join()
-            print('pool cleaned')
+        
+        # Use multiprocessing
+        if 1:
+
+            # Applying the multiple processing here, with process of choice, i use 4 for local and 16 for lustre 
+            # Here we pass the arguments to the initializer for pool so we use in the function used in multiple processing, it can be changed differently 
+            with Pool(processes=param.nproc, initializer=init_worker_nc, initargs=(var_dict, in_file, outpath_final)) as pool:
+                # I am not returning results, usually you can return and write the results after multiprocessing
+                print('pool started')
+                results = pool.starmap(processInput_trends, [(i) for i in result_chunks])
+                pool.close()
+                pool.join()
+                print('pool cleaned')
+
+        ## or so it sequentially
+        else:
+
+            init_worker_nc(var_dict, in_file, outpath_final)
+            for i in result_chunks:
+                processInput_trends(*i)
+
 
         # increase the main block iteration
         main_block_iteration += 1
 
 
-def compute_trends(*largs):
+def compute_trends(*args):
     # Lock is the module from multiprocessing to allow the write of netcdf files one at a time to avoid conflict, first invocation here in main
 
     global lock 
@@ -253,18 +303,19 @@ def compute_trends(*largs):
     var_dict = {}
 
     class ArgsTmp:
-        def __init__(self, h, inp, outp, prod, chunks, np):
+        def __init__(self, h, inp, outp, prod, chunks, np, b_delete):
             self.hash = h
             self.input = inp
             self.output = outp
             self.product_tag = prod
             self.nproc = np
             self.chunks = chunks
+            self.b_delete = b_delete
 
 
 
     global param 
-    param = ArgsTmp(*largs)
+    param = ArgsTmp(*args)
     main()
 
 