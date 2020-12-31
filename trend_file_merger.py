090c5a4232550ea6103f1d83692a06209dfe85fb
remy fransen <remy.fransen@emailprovider.fr>
2020-12-31 18:46:22 +0100

    Speed up plot function

--- trend_file_merger.py
+++ trend_file_merger.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import os,sys
 import fnmatch
 import pathlib
+from timeit import default_timer as timer
 
 def pprinttable(rows, header, fmt):
     w = 9
@@ -127,11 +128,21 @@ def merge_trends(product, chunks, config, phash):
         plt.savefig('tile_map_loc.png')
     
 
-def plot_trends(product, chunks, plot_name, plot_choice, scale_tendency, config, phash):
+def plot_trends_OLD(product, chunks, plot_name, plot_choice, scale_tendency, config, phash):
+    """deprecated version, too slow on loading data"""
     
+    t0 = timer()
+
     import cartopy.crs as ccrs
+    
+    print('t000', timer()-t0)
+    t0 = timer()
+
     import xarray as xr
     
+    print('t00', timer()-t0)
+    t0 = timer()
+
     input_trend_file = pathlib.Path(config['output_path']['trend']) / product / config['output_path']['merged_filename']
     output_path = pathlib.Path(config['output_path']['plot']) / product
 
@@ -146,6 +157,9 @@ def plot_trends(product, chunks, plot_name, plot_choice, scale_tendency, config,
     ## Read merged data
     nc_output = Dataset(input_trend_file, 'r')
 
+    print('t01', timer()-t0)
+    t0 = timer()
+
     trends = {}
     trends['pval'] = nc_output.variables['chunk_scores_p_val'][:]
     trends['zval'] = nc_output.variables['chunk_scores_z_val'][:]
@@ -153,6 +167,9 @@ def plot_trends(product, chunks, plot_name, plot_choice, scale_tendency, config,
     trends['sn'] = nc_output.variables['chunk_scores_Sn_val'][:]
     nc_output.close()
     
+    print('t1', timer()-t0)
+    t0 = timer()
+
     ## Read MSG geographical data
     work_dir_msg = './input_ancillary/'
 
@@ -162,22 +179,34 @@ def plot_trends(product, chunks, plot_name, plot_choice, scale_tendency, config,
     lonmsg = h5py.File(work_dir_msg+'HDF5_LSASAF_MSG_LON_MSG-Disk_4bytesPrecision','r')
     lon_MSG = 0.0001*lonmsg['LON'][:]
     
+    print('t10', timer()-t0)
+    t0 = timer()
+
     hangle_view_zen = h5py.File(work_dir_msg+'GEO_L1B-ANGLES-MSG2_2012-06-15T13-00-00_V1-00.hdf5','r')
     view_zenith = hangle_view_zen['View_Zenith'][:]
     view_zenith = np.array(view_zenith,dtype='float')
     view_zenith[view_zenith==65535] = np.NaN
     view_zenith = view_zenith*0.01
     
+    print('t11', timer()-t0)
+    t0 = timer()
+
     trends['sn'][trends['sn']==999] = np.NaN
     trends['sn'][trends['pval']>0.05] = np.NaN
 
     trends['zval'][trends['zval']==999] = np.NaN
     trends['zval'][trends['pval']>0.05] = np.NaN
 
+    print('t2', timer()-t0)
+    t0 = timer()
+
     ## Plot 
     proj = ccrs.Geostationary()
     #fig, ax = plt.subplots(figsize=(12, 12), subplot_kw={'projection':proj})
     fig, ax = plt.subplots(subplot_kw={'projection':proj})
+
+    print('t20', timer()-t0)
+    t0 = timer()
 
 
     if plot_choice=='sn':
@@ -189,6 +218,9 @@ def plot_trends(product, chunks, plot_name, plot_choice, scale_tendency, config,
     
     dx.coords['lat'] = (('y', 'x'), lat_MSG[zone_bnd])
     dx.coords['lon'] = (('y', 'x'), lon_MSG[zone_bnd])
+
+    print('t21', timer()-t0)
+    t0 = timer()
 
     cm = 'jet'
     #cm = 'nipy_spectral'
@@ -211,6 +243,124 @@ def plot_trends(product, chunks, plot_name, plot_choice, scale_tendency, config,
         imm = dx.plot(x='lon', y='lat', transform=ccrs.PlateCarree(), cmap=cm, add_colorbar=False, extend='neither', ax=ax)
 
 
+    print('t22', timer()-t0)
+    t0 = timer()
+
+    cbar = plt.colorbar(imm,ax=ax,orientation='horizontal',shrink=.8,pad=0.05,aspect=10)
+    cbar.ax.tick_params(labelsize=14,rotation=90)
+
+    ax.coastlines()
+    plt.axis('off')
+    ax.set_title('{} for {}'.format(plot_choice, plot_name.split(':')[1]), fontsize=20)
+    
+    zone_str = '_'.join(chunks.get_limits('global','str'))
+    plot_fname = phash+'_'+plot_choice+'_'+zone_str+'.png'
+    plot_path = output_path / plot_fname
+    
+    print('t3', timer()-t0)
+    t0 = timer()
+
+    #fig.savefig(plot_string_save, dpi=300)
+    fig.savefig(plot_path, dpi=100)
+    plt.close()   
+
+    print('*** IMAGE SAVED >', plot_path.as_posix())
+
+    print('t4', timer()-t0)
+    t0 = timer()
+
+
+
+
+
+def plot_trends(product, chunks, plot_name, plot_choice, scale_tendency, config, phash):
+    """
+    Faster version: load only required data, not the full disk each time.
+    import xarray is still long (several second) but only at the first call
+    """
+    
+    import cartopy.crs as ccrs
+    import xarray as xr
+    
+    input_trend_file = pathlib.Path(config['output_path']['trend']) / product / config['output_path']['merged_filename']
+    output_path = pathlib.Path(config['output_path']['plot']) / product
+
+    # Shortcut to [ylim1:ylim2,xlim1:xlim2]
+    zone_bnd = chunks.get_limits('global', 'slice')
+
+    # Make dir if not exists
+    output_path.mkdir(parents=True, exist_ok=True)
+    
+    print('*** PLOT TRENDS')
+    
+    ## Read merged data
+    nc_output = Dataset(input_trend_file, 'r')
+
+    trends = {}
+    trends['pval'] = nc_output.variables['chunk_scores_p_val'][zone_bnd]
+    trends['zval'] = nc_output.variables['chunk_scores_z_val'][zone_bnd]
+    trends['len'] = nc_output.variables['chunk_scores_length'][zone_bnd]
+    trends['sn']  = nc_output.variables['chunk_scores_Sn_val'][zone_bnd]
+    nc_output.close()
+    
+
+    ## Read MSG geographical data
+    work_dir_msg = './input_ancillary/'
+
+    latmsg = h5py.File(work_dir_msg+'HDF5_LSASAF_MSG_LAT_MSG-Disk_4bytesPrecision','r')
+    lat_MSG = 0.0001*latmsg['LAT'][zone_bnd]
+
+    lonmsg = h5py.File(work_dir_msg+'HDF5_LSASAF_MSG_LON_MSG-Disk_4bytesPrecision','r')
+    lon_MSG = 0.0001*lonmsg['LON'][zone_bnd]
+    
+    hangle_view_zen = h5py.File(work_dir_msg+'GEO_L1B-ANGLES-MSG2_2012-06-15T13-00-00_V1-00.hdf5','r')
+    view_zenith = hangle_view_zen['View_Zenith'][zone_bnd]
+    view_zenith = np.array(view_zenith,dtype='float')
+    view_zenith[view_zenith==65535] = np.NaN
+    view_zenith = view_zenith*0.01
+    
+    trends['sn'][trends['sn']==999] = np.NaN
+    trends['sn'][trends['pval']>0.05] = np.NaN
+
+    trends['zval'][trends['zval']==999] = np.NaN
+    trends['zval'][trends['pval']>0.05] = np.NaN
+
+
+    ## Plot 
+    proj = ccrs.Geostationary()
+    #fig, ax = plt.subplots(figsize=(12, 12), subplot_kw={'projection':proj})
+    fig, ax = plt.subplots(subplot_kw={'projection':proj})
+
+    if plot_choice=='sn':
+        trends[plot_choice][view_zenith>75] = np.NaN
+        dx = xr.DataArray(trends[plot_choice]*scale_tendency, dims = ('y','x'))
+        # The value of scale tendency depends on the product, daily or high frequency, for daily, the value is 365
+    else:
+        dx = xr.DataArray(trends[plot_choice], dims = ('y','x'))
+    
+    dx.coords['lat'] = (('y', 'x'), lat_MSG)
+    dx.coords['lon'] = (('y', 'x'), lon_MSG)
+
+    cm = 'jet'
+    #cm = 'nipy_spectral'
+    #cm = 'RdBu_r'
+
+    if plot_choice=='sn':
+        #imm = dx.plot(x='lon', y='lat',transform=ccrs.PlateCarree(), vmin=-1E-5, vmax=1E-5, cmap='RdBu_r', add_colorbar=False, extend='neither', ax=ax)
+        imm = dx.plot(x='lon', y='lat',transform=ccrs.PlateCarree(), vmin=-1E-1, vmax=1E-1, cmap=cm, add_colorbar=False, extend='neither', ax=ax)
+
+
+    if plot_choice=='zval':
+        imm = dx.plot(x='lon', y='lat', transform=ccrs.PlateCarree(), vmin=-10, vmax=10, cmap=cm, add_colorbar=False, extend='neither', ax=ax)
+        #imm = dx.plot(x='lon', y='lat', transform=ccrs.PlateCarree(), cmap='RdBu_r', add_colorbar=False, extend='neither', ax=ax)
+
+    if plot_choice=='pval':
+        imm = dx.plot(x='lon', y='lat', transform=ccrs.PlateCarree(), vmin=0., vmax=1., cmap=cm, add_colorbar=False, extend='neither', ax=ax)
+        #imm = dx.plot(x='lon', y='lat', transform=ccrs.PlateCarree(), cmap='RdBu_r', add_colorbar=False, extend='neither', ax=ax)
+
+    if plot_choice=='len':
+        imm = dx.plot(x='lon', y='lat', transform=ccrs.PlateCarree(), cmap=cm, add_colorbar=False, extend='neither', ax=ax)
+
     cbar = plt.colorbar(imm,ax=ax,orientation='horizontal',shrink=.8,pad=0.05,aspect=10)
     cbar.ax.tick_params(labelsize=14,rotation=90)
 
@@ -228,11 +378,6 @@ def plot_trends(product, chunks, plot_name, plot_choice, scale_tendency, config,
 
     print('*** IMAGE SAVED >', plot_path.as_posix())
 
-
-
-
-
-    
 
 
 
