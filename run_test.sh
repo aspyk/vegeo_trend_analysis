5e474ef5dbf0b463b5e359705e2a8b6f5fe3bf1d
Remy Fransen <remymf@uservm.terrascope.be>
2021-03-01 16:11:00 +0100

    Update test script

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,3 @@
 f2py -c mk_fortran.f90 -m mankendall_fortran_repeat_exp2 --fcompiler=gfortran
-python main.py -t0 1981-01-01 -t1 1981-12-31 -i latloncsv:config -p c3s_al_bbdh_AVHRR  -a extract merge trend plot -d
+#python main.py -t0 1981-01-01 -t1 1981-12-31 -i latloncsv:config -p c3s_al_bbdh_AVHRR  -a extract merge trend plot -d
+python main.py -t0 1981-01-01 -t1 2020-12-31 -i latloncsv:config -p c3s_al_bbdh_AVHRR c3s_al_bbdh_VGT c3s_al_bbdh_PROBAV -a extract merge trend plot -d
